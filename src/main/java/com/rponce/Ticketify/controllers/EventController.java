b331241cd2ac98b288973a71068f55dcedfebdee
Luis Alonso <62676424+alonso-rb@users.noreply.github.com>
2023-07-04 16:14:07 -0600

    Update EventController.java

--- src/main/java/com/rponce/Ticketify/controllers/EventController.java
+++ src/main/java/com/rponce/Ticketify/controllers/EventController.java
@@ -43,7 +43,7 @@ public class EventController {
 	@Autowired
 	private TierService tierService;
 	
-	@GetMapping("/")
+	@GetMapping("/all")
 	public ResponseEntity<?> findAllEvents(@RequestParam(defaultValue = "") String title,
 			@RequestParam(defaultValue = "0") int page,
 			@RequestParam(defaultValue = "10") int size){
