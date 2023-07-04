303fa30f704aadaf106701c225b4eeb418d13cf7
Luis Alonso <62676424+alonso-rb@users.noreply.github.com>
2023-07-04 17:05:10 -0600

    Update UserInfoDTO.java

--- src/main/java/com/rponce/Ticketify/models/dtos/UserInfoDTO.java
+++ src/main/java/com/rponce/Ticketify/models/dtos/UserInfoDTO.java
@@ -11,7 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class UserInfoDTO {
 	
-	private List<ReturnUserRoleDTO> RoleName;
+	private List<String> RoleName;
 	
 	private String token;
 
